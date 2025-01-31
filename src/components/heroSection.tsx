b45183a326ef738a96422c33e5a72be5f6a7ead5
Raja Waqas <rajawaqas912@gmail.com>
2025-01-31 18:37:30 +0500

    feat(Responsive)   hero section responsive for sm devices.

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -49,44 +49,45 @@ const HeroSection = () => {
 
         <div className="bg-hero-section-lower bg-no-repeat bg-cover h-[227px] bg-end absolute bottom-0 left-0 right-0"></div>
 
-        {/* <div className="bg-hero-section-logo w-[891px] h-[462px] bg-no-repeat bg-contain absolute left-[50%] -translate-x-[50%] top-[10%]"></div> */}
+        <div className="bg-hero-section-logo lg:w-[891px] lg:h-[462px] w-[288px] h-[149px] bg-no-repeat bg-contain absolute left-[50%] -translate-x-[50%] lg:top-[10%] top-[13%]"></div>
 
         {/* casltes  red*/}
-        <div className="bg-hero-section-castle-red-1 lg:w-[344px] lg:h-[435px] w-[132px] h-[166px] bg-contain bg-no-repeat absolute z-0 md:top-[16%] top-[19%] md:left-[0%] -left-12"></div>
-        <div className="bg-hero-section-frog lg:w-[211px] lg:h-[230px] h-[69px] w-[61px] bg-contain z-10 absolute md:top-[30%] top-[35%] md:left-0 -left-4"></div>
-        <div className="bg-hero-section-chillguy md:w-[352px] md:h-[341px] w-[117px] h-[111px] bg-contain z-10 absolute md:top-[26%] top-[31%] md:left-[9%] left-[7%]"></div>
-        <div className="bg-hero-section-castle-red-2 lg:w-[456px] lg:h-[619px] h-[220px] w-[162px] bg-contain bg-no-repeat absolute top-[40%] z-30 md:left-[0%] -left-12"></div>
+        <div className="bg-hero-section-castle-red-1 lg:w-[344px] lg:h-[435px] w-[132px] h-[166px] bg-contain bg-no-repeat absolute z-0 md:top-[16%] top-[25%] md:left-[0%] -left-12"></div>
+        <div className="bg-hero-section-frog lg:w-[211px] lg:h-[230px] h-[69px] w-[61px] bg-contain z-10 absolute md:top-[30%] top-[41%] md:left-0 -left-4"></div>
+        <div className="bg-hero-section-chillguy md:w-[352px] md:h-[341px] w-[117px] h-[111px] bg-contain z-10 absolute md:top-[26%] top-[37%] md:left-[9%] left-[7%]"></div>
+        <div className="bg-hero-section-castle-red-2 lg:w-[456px] lg:h-[619px] h-[220px] w-[162px] bg-contain bg-no-repeat absolute top-[46%] z-30 md:left-[0%] -left-12"></div>
         <div className="bg-hero-section-memcoin-1 w-[273px] h-[319px]  absolute top-[54%] z-20 md:block hidden"></div>
-        <div className="bg-black lg:w-[356px] lg:h-[500px] absolute top-[48%] h-[166px] w-[110px] z-10 md:left-[0%] -left-12"></div>
+        <div className="bg-black lg:w-[356px] lg:h-[500px] absolute top-[53%] h-[166px] w-[110px] z-10 md:left-[0%] -left-12"></div>
 
-        <div className="bg-hero-section-red-1 w-[116px] h-[128px] absolute top-[90%] left-[30%] z-10"></div>
-        <div className="bg-hero-section-red-2 w-[96px] h-[108px] absolute top-[87%] z-10"></div>
+        <div className="bg-hero-section-red-1 lg:w-[116px] lg:h-[128px] w-[50px] h-[54px]  bg-contain bg-no-repeat  absolute lg:top-[90%] top-[85%] lg:left-[30%] left-[5%] z-10"></div>
+        <div className="bg-hero-section-red-2 w-[96px] h-[108px] absolute top-[87%] z-10 hidden md:block"></div>
 
-        <div className="bg-hero-section-shark-1 w-[104px] h-[114px] absolute top-[85%] right-[25%] z-10"></div>
-        <div className="bg-hero-section-shark-2 w-[104px] h-[114px] absolute top-[90%] right-[2%] z-10"></div>
+        <div className="bg-hero-section-shark-1 lg:w-[104px] lg:h-[114px] w-[52px] h-[57px]  bg-contain bg-no-repeat  absolute top-[85%] right-[25%] z-10"></div>
+        <div className="bg-hero-section-shark-2 w-[104px] h-[114px] absolute top-[90%] right-[2%] hidden md:block z-10"></div>
 
-        {/* <div className="bg-hero-section-main w-[680px] h-[582px] absolute top-[52%] left-[33%] z-10"></div>
+        <div className="bg-hero-section-main w-[680px] h-[582px] absolute top-[52%] left-[33%] z-10 hidden md:block"></div>
+
         <div
           ref={catRef}
-          className="bg-hero-section-memcoin-2 w-[431px] h-[545px]  absolute top-[58%] z-[31] left-[3%]"
+          className="bg-hero-section-memcoin-2 lg:w-[431px] lg:h-[545px] w-[144px] h-[180px] bg-contain bg-no-repeat  absolute lg:top-[64%] top-[68%] z-[31] lg:left-[3%] left-[15%]"
           onMouseDown={playPopCatSound}
           onMouseUp={closeCatMouth}
           onMouseLeave={closeCatMouth}
-        ></div> */}
-        {/* <div className="bg-hero-section-memcoin-3 w-[418px] h-[504px]  absolute top-[52%] left-[15%] z-30"></div> */}
-        {/* <div className="bg-hero-section-distant w-[1208px] h-[608px]  absolute top-[42%] left-[19%] bg-no-repeat bg-contain z-0"></div> */}
+        ></div>
+        <div className="bg-hero-section-memcoin-3 lg:w-[418px] lg:h-[504px] w-[147px] h-[176px] bg-contain bg-no-repeat  absolute top-[58%] lg:left-[15%] left-[0%] z-30"></div>
+        <div className="bg-hero-section-distant lg:w-[1208px] lg:h-[608px] w-[300px] h-[105px] bg-contain bg-no-repeat   absolute lg:top-[42%] top-[55%]  left-[19%] z-0"></div>
 
         {/* castle-blue */}
-        <div className="bg-hero-section-castle-blue-1 lg:w-[344px] lg:h-[475px] h-[181px] w-[134px] bg-contain bg-no-repeat absolute z-0 md:top-[16%] top-[19%] md:right-0 -right-12"></div>
-        <div className="bg-hero-section-memcoin-4 lg:w-[199px] lg:h-[229px] w-[71px] h-[82px] bg-contain bg-no-repeat z-10 absolute md:top-[30%] top-[35%] md:right-0 -right-4"></div>
-        <div className="bg-hero-section-memcoin-8 lg:w-[401px] lg:h-[403px] w-[133px] h-[131px] bg-contain bg-no-repeat z-10 absolute md:top-[26%] top-[29%] md:right-[6%] right-[4%]"></div>
-        <div className="bg-hero-section-castle-blue-2 lg:w-[446px] lg:h-[606px] absolute top-[40%] h-[221px] w-[168px] bg-contain bg-no-repeat z-30 md:right-0 -right-12"></div>
-        {/* 
-        <div className="bg-hero-section-memcoin-1 w-[273px] h-[319px]  absolute top-[54%] z-20"></div>
-        <div className="bg-black w-[356px] h-[500px] absolute top-[48%] z-10"></div>
-        <div className="bg-hero-section-memcoin-5 w-[346px] h-[432px] z-30 absolute top-[56%] right-[15%]"></div>
-        <div className="bg-hero-section-memcoin-6 w-[350px] h-[434px] z-[31] absolute top-[60%] right-[6%]"></div>
-        <div className="bg-hero-section-memcoin-7 w-[299px] h-[351px] z-30 absolute top-[63%] -right-[3%]"></div> */}
+        <div className="bg-hero-section-castle-blue-1 lg:w-[344px] lg:h-[475px] h-[181px] w-[134px] bg-contain bg-no-repeat absolute z-0 md:top-[16%] top-[25%] md:right-0 -right-12"></div>
+        <div className="bg-hero-section-memcoin-4 lg:w-[199px] lg:h-[229px] w-[71px] h-[82px] bg-contain bg-no-repeat z-10 absolute md:top-[30%] top-[41%] md:right-0 -right-4"></div>
+        <div className="bg-hero-section-memcoin-8 lg:w-[401px] lg:h-[403px] w-[133px] h-[131px] bg-contain bg-no-repeat z-10 absolute md:top-[26%] top-[35%]  right-[4%]"></div>
+        <div className="bg-hero-section-castle-blue-2 lg:w-[446px] lg:h-[606px] absolute top-[46%] h-[221px] w-[168px] bg-contain bg-no-repeat z-30 md:right-0 -right-12"></div>
+
+        <div className="bg-hero-section-memcoin-1 w-[273px] h-[319px]  absolute top-[54%] z-20 hidden md:block"></div>
+
+        <div className="bg-hero-section-memcoin-5 lg:w-[346px] lg:h-[432px] w-[137px] h-[171px] bg-contain bg-no-repeat z-30 absolute top-[56%]  right-[15%]"></div>
+        <div className="bg-hero-section-memcoin-6 lg:w-[350px] lg:h-[434px] w-[119px] h-[147px] bg-contain bg-no-repeat z-[31]  absolute top-[68%] lg:right-[6%] right-[0%]"></div>
+        <div className="bg-hero-section-memcoin-7 w-[299px] h-[351px] z-30 absolute top-[63%] -right-[3%] hidden md:block "></div>
       </div>
     </div>
   );
